edb5920e774d60c07d35dbb635057427d1363cb4
Alexander Skrock <alexanderskrock@gmx.de>
2024-06-11 21:00:22 +0200

    feat(entry-page) render icons for circle selector

--- src/pages/entry/CircleSelector.js
+++ src/pages/entry/CircleSelector.js
@@ -2,15 +2,60 @@ import React, { useMemo } from "react";
 
 import { PieChart } from "react-minimal-pie-chart";
 
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+
+    return {
+        x: centerX + (radius * Math.cos(angleInRadians)),
+        y: centerY + (radius * Math.sin(angleInRadians))
+    };
+}
+  
+function describeArc(x, y, radius, startAngle, endAngle){
+    const start = polarToCartesian(x, y, radius, endAngle);
+    const end = polarToCartesian(x, y, radius, startAngle);
+    const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+    return `M ${start.x} ${start.y} A ${radius} ${radius} 0 ${largeArcFlag} 0 ${end.x} ${end.y}`;     
+}
+
+const iconRenderer = ({ x, y, dataEntry: { degrees, iconRef, startAngle, title }}) => {
+    // TODO calculate radious
+    const radius = 50;
+    // TODO Explore why this is needed (possibly the pie char library uses another zero-orientation?!)
+    const angleCorrection = 90;
+    return (
+        <svg>
+            <defs>
+                <pattern id={ `pattern-${title}` } width="100%" height="100%" patternContentUnits="objectBoundingBox">
+                    <image x="0.25" y="0.25" width="0.5" height="0.5" href={ iconRef } />
+                </pattern>
+            </defs>
+            <path
+                d={
+                    `M ${x} ${y}
+                    ${describeArc(x, y, radius, startAngle + angleCorrection, startAngle + degrees + angleCorrection)}
+                    L ${x} ${y}`
+                }
+                fill={ `url(#pattern-${title})` } />
+        </svg>
+    );
+}
+
+const labelRenderer = ({ dataEntry, ...rest }) => {
+    if (dataEntry.iconRef) {
+        return iconRenderer({ dataEntry, ...rest });
+    }
+    return dataEntry.label;
+}
+
 const CircleSelector = ({ options, onSelect }) => {
     const adaptedOptions = useMemo(() => options ? options.map(option => ({
         value: 100 / options.length,
         ...option,
     })) : [], [options]);
 
-    return (
-        <PieChart data={ adaptedOptions } onClick={ (e, i) => onSelect(adaptedOptions[i]) } label={ ({ dataEntry }) => dataEntry.label } />
-    )
+    return <PieChart data={ adaptedOptions } onMouseOver={ (e, i) => onSelect(adaptedOptions[i]) } label={ labelRenderer } />;
 }
 
 export default CircleSelector;
--- src/pages/entry/EntryPage.js
+++ src/pages/entry/EntryPage.js
@@ -7,31 +7,37 @@ import FullPageBox from "../../components/FullPageBox";
 import FullPageImage from "../../components/FullPageImage";
 import FullPageContent from "../../components/FullPageContent";
 
+import business from "./business.svg";
+import cat from "./cat.svg";
+import dog from "./dog.svg";
+import family from "./family.svg";
+import hobby from "./hobby.svg";
+
 const options = [
     {
         color: "#a3c8a3",
         title: "Work",
-        label: "Work",
+        iconRef: business,
     },
     {
         color: "#73ab73",
         title: "Hobby",
-        label: "Hobby",
+        iconRef: hobby,
     },
     {
         color: "#4e834e",
         title: "Family",
-        label: "Family",
+        iconRef: family,
     },
     {
         color: "#bfd9bf",
         title: "Dog",
-        label: "Dog",
+        iconRef: dog,
     },
     {
         color: "#5e9c5e",
         title: "Cats",
-        label: "Cats",
+        iconRef: cat,
     },
 ];
 
